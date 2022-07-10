dadb7bdb33294309b44eee36b9b72c49d60994fe
Jimbanan <uservice371@mail.ru>
2022-07-10 20:55:46 +0300

    Create DealClient

--- src/main/java/com/neoflex/gateway/feignclient/DealClient.java
+++ src/main/java/com/neoflex/gateway/feignclient/DealClient.java
@@ -1,16 +1,35 @@
 package com.neoflex.gateway.feignclient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.neoflex.gateway.dto.ApplicationDTO;
+import com.neoflex.gateway.dto.FinishRegistrationRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @FeignClient(name = "dealMC", url = "${deal.hostname}")
 public interface DealClient {
 
     @PutMapping("deal/calculate/{applicationId}")
-    ResponseEntity<?> choosingApplication(@RequestBody FinishRegistrationRequestDTO finishRegistrationRequestDTO,
-                                          @PathVariable Long applicationId);
+    ResponseEntity<?> calculate(@RequestBody FinishRegistrationRequestDTO finishRegistrationRequestDTO,
+                                @PathVariable Long applicationId);
+
+    @PostMapping("deal/document/{applicationId}/send")
+    ResponseEntity<?> sendDocs(@PathVariable Long applicationId);
+
+    @PostMapping("deal/document/{applicationId}/sign")
+    ResponseEntity<?> singDocs(@PathVariable Long applicationId);
+
+    @PostMapping("deal/document/{applicationId}/code")
+    ResponseEntity<?> receiveSesCode(@PathVariable Long applicationId,
+                                     @RequestBody Integer sesCode);
+
+    @GetMapping("/deal/admin/application/{applicationId}")
+    ApplicationDTO getOffersByID(@PathVariable Long applicationId);
+
+    @GetMapping("/deal/admin/application")
+    List<ApplicationDTO> getAllOffers();
 
 }
