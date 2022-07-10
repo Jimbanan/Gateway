eb42dead0a53f9f88b37ce2d7ff042cacb865aa2
Jimbanan <uservice371@mail.ru>
2022-07-10 20:56:18 +0300

    Create new enums

---
+++ src/main/java/com/neoflex/gateway/dto/PaymentScheduleElement.java
@@ -0,0 +1,26 @@
+package com.neoflex.gateway.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Сущность графика платежей")
+public class PaymentScheduleElement {
+
+    private Integer number;
+    private LocalDate date;
+    private BigDecimal totalPayment;
+    private BigDecimal interestPayment;
+    private BigDecimal debtPayment;
+    private BigDecimal remainingDebt;
+
+}
