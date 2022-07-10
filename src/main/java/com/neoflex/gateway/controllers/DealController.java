9621a33900edd1491904f38709c9e246894e800a
Jimbanan <uservice371@mail.ru>
2022-07-10 21:14:07 +0300

    All the necessary APIs for implementing the Gateway API have been created

--- src/main/java/com/neoflex/gateway/controllers/DealController.java
+++ src/main/java/com/neoflex/gateway/controllers/DealController.java
@@ -1,18 +1,14 @@
 package com.neoflex.gateway.controllers;
 
-import com.neoflex.gateway.dto.LoanApplicationRequestDTO;
-import com.neoflex.gateway.dto.LoanOfferDTO;
-import com.neoflex.gateway.feignclient.ApplicationClient;
+import com.neoflex.gateway.dto.FinishRegistrationRequestDTO;
+import com.neoflex.gateway.feignclient.DealClient;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -20,10 +16,40 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DealController {
 
-    @Operation(description = "Создание заявки на кредит") //Первый
-    @PostMapping("/DealController")
-    public ResponseEntity<?> createDeal() {
-        return null;
+    private final DealClient dealClient;
+
+    @Operation(description = "Расчет по выбранной заявке") //Третий
+    @PostMapping("/calculate/{applicationId}")
+    public ResponseEntity<?> finishRegistration(@RequestBody FinishRegistrationRequestDTO finishRegistrationRequestDTO,
+                                                @PathVariable(value = "ID оформляемой заявки") Long applicationId) {
+        log.info("finishRegistration() - ResponseEntity<?>: Создание заявки на кредит");
+        dealClient.calculate(finishRegistrationRequestDTO, applicationId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(description = "Запрос на отправку документов")//Четвертый
+    @PostMapping("/document/{applicationId}/send")
+    public ResponseEntity<?> sendDocs(@PathVariable(value = "ID оформляемой заявки") Long applicationId) {
+        log.info("sendDocs() - ResponseEntity<?>: Запрос на отправку документов");
+        dealClient.sendDocs(applicationId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(description = "Запрос на подписание документов") //Пятый
+    @PostMapping("/document/{applicationId}/sign")
+    public ResponseEntity<?> singDocs(@PathVariable(value = "ID оформляемой заявки") Long applicationId) {
+        log.info("singDocs() - ResponseEntity<?>: Запрос на подписание документов");
+        dealClient.singDocs(applicationId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(description = "Подписание документов") //Шестой
+    @PostMapping("/document/{applicationId}/code")
+    public ResponseEntity<?> receiveSesCode(@PathVariable(value = "ID оформляемой заявки") Long applicationId,
+                                            @RequestBody Integer sesCode) {
+        log.info("receiveSesCode() - ResponseEntity<?>: Подписание документов");
+        dealClient.receiveSesCode(applicationId, sesCode);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
